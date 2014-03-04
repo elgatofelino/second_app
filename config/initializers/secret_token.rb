fbb4d2290d872851afe6660088138d4dcc028e58
elgatofelino <elgatofelino@gmail.com>
2014-03-04 18:56:23 +0100

    Repositorio inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '02a775f4fb2bd6348c85160e1e3d88febc20cc0c9aa657b338693f5121d1f23aa2d031b32033ede7923c052d4ef8e5302e3b6833dcf753b5e285024bfe064ba8'
